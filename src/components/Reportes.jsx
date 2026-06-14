c03dde4b1fd9bbbdbd3911243b81524c1d828f2c
SITT-TIJUANA <gjvazquez@tijuana.gob.mx>
2026-06-14 11:25:19 -0700

    Update Reportes.jsx

--- src/components/Reportes.jsx
+++ src/components/Reportes.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from 'react';
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
